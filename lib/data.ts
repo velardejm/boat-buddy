6c4128700db3f44d3bb4358ec2956dc2422099e3
jmyv08 <velarde.jm08@gmail.com>
2025-01-30 11:53:23 +0300

    Add user to db now works

--- lib/data.ts
+++ lib/data.ts
@@ -2,7 +2,7 @@
 
 // import { db, sql } from "@vercel/postgres";
 import { sql } from "@vercel/postgres";
-// import bcrypt from "bcrypt";
+import bcrypt from "bcrypt";
 
 // const client = await db.connect();
 
@@ -19,20 +19,21 @@ export async function getUsers() {
 }
 
 export async function addUser(formData: FormData) {
-  const username = formData.get("username");
-  const password = formData.get("password");
+  const username = formData.get("username") as string;
+  const password = formData.get("password") as string;
+  console.log(typeof password);
+  const hashedPassword = await bcrypt.hash(password, 10);
   console.log("username: " + username);
-  console.log("password: " + password);
+  console.log("password: " + hashedPassword);
   try {
-    // const hashedPassowrd = await bcrypt.hash(formData.password, 10);
     // const data = await client.sql`
-    // const data = await sql`
-    //   INSERT INTO users (username, email, password)
-    //   VALUES (${username}, ${password})
-    //   RETURNING username
-    // `;
+    const data = await sql`
+      INSERT INTO users (username, password)
+      VALUES (${username}, ${hashedPassword})
+      RETURNING username
+    `;
     // console.log("========================");
-    // console.log(data);
+    console.log(data);
     // return data; // Returning the username of the newly added user
   } catch (error) {
     console.log(error);
