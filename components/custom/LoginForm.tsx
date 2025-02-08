b5af5d5399dc7f7ba7e50f37120b2a6fdc10f098
JM Velarde <velarde.jmv8@gmail.com>
2025-02-08 16:27:11 +0300

    Add log in

--- components/custom/LoginForm.tsx
+++ components/custom/LoginForm.tsx
@@ -4,6 +4,10 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
+import { useRouter } from "next/navigation";
+import { login } from "@/lib/data";
+import { useActionState, useEffect } from "react";
+
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -20,16 +24,37 @@ const formSchema = z.object({
   username: z.string().min(2, {
     message: "Username must be at least 2 characters.",
   }),
-  password: z.string(),
+  password: z.string().min(2, {
+    message: "Password must be at least 2 characters.",
+  }),
 });
 
 export default function LoginForm() {
+  const router = useRouter();
+
   // 1. Define your form.
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       username: "",
+      password: "",
     },
+  });
+
+  const [state, formAction, pending] = useActionState(login, {
+    message: "",
+    success: false,
+  });
+
+  useEffect(() => {
+    if (state.success) {
+      router.push("/");
+    }
+  }, [state]);
+
+  const parse = formSchema.safeParse({
+    username: form.getValues("username"),
+    password: form.getValues("password"),
   });
 
   // 2. Define a submit handler.
@@ -42,7 +67,8 @@ export default function LoginForm() {
   return (
     <Form {...form}>
       <form
-        onSubmit={form.handleSubmit(onSubmit)}
+        // onSubmit={form.handleSubmit(onSubmit)}
+        action={formAction}
         className="space-y-8 max-w-1/2 min-w-1/4 md:w-1/4 flex flex-col  items-center h-1/2"
       >
         <h1 className="w-full text-3xl font-bold relative -left-4">Log In</h1>
@@ -50,7 +76,10 @@ export default function LoginForm() {
           control={form.control}
           name="username"
           render={({ field }) => (
-            <FormItem className="w-full">
+            <FormItem
+              className="w-full"
+              onChange={() => form.trigger("username")}
+            >
               {/* <FormLabel>Username</FormLabel> */}
               <FormControl>
                 <Input placeholder="Username" {...field} />
@@ -64,7 +93,10 @@ export default function LoginForm() {
           control={form.control}
           name="password"
           render={({ field }) => (
-            <FormItem className="w-full">
+            <FormItem
+              className="w-full"
+              onChange={() => form.trigger("password")}
+            >
               {/* <FormLabel>Password</FormLabel> */}
               <FormControl>
                 <Input placeholder="Password" type="password" {...field} />
@@ -73,8 +105,10 @@ export default function LoginForm() {
             </FormItem>
           )}
         />
-
-        <Button type="submit">Submit</Button>
+        <p>{state.message}</p>
+        <Button type="submit" disabled={!parse.success || pending}>
+          {pending ? "Submitting..." : "Submit"}
+        </Button>
       </form>
     </Form>
   );
--- lib/data.ts
+++ lib/data.ts
@@ -38,12 +38,71 @@ export async function addUser(
         RETURNING userId
       `;
 
-    await createSession(data.rows[0].userId);
+    // await createSession(data.rows[0].userId);
 
     return { message: "Signup Successful", success: true };
   } catch (error) {
     return {
       message: "Signup failed, please try again later.",
+      success: false,
+    };
+  }
+}
+
+export async function login(
+  prevState: { message: string; success: boolean },
+  formData: FormData
+) {
+  // const username = formData.get("username") as string;
+  // const password = formData.get("password") as string;
+  // const userExists = await sql`
+  // SELECT 1 FROM users WHERE username = ${username} LIMIT 1
+  // `;
+
+  // if (userExists.rowCount === 0) {
+  //   return { message: "Username not found.", success: false };
+  // }
+
+  // const result = await sql`
+  // SELECT userId, password FROM users WHERE username = ${username} LIMIT 1
+  // `;
+  // const { password: hashedPassword, userId } = result.rows[0];
+
+  // const isPasswordCorrect = await bcrypt.compare(password, hashedPassword);
+  // if (isPasswordCorrect) {
+  //   await createSession(userId);
+  //   return { message: "Successfully logged in.", success: true };
+  // } else {
+  //   return {
+  //     message: "Log in failed. Please try again later.",
+  //     success: false,
+  //   };
+  // }
+
+  try {
+    const username = formData.get("username") as string;
+    const password = formData.get("password") as string;
+    const userExists = await sql`
+    SELECT 1 FROM users WHERE username = ${username} LIMIT 1
+    `;
+
+    if (userExists.rowCount === 0) {
+      return { message: "Username not found.", success: false };
+    }
+
+    const result = await sql`
+    SELECT userId, password FROM users WHERE username = ${username} LIMIT 1
+    `;
+    const { password: hashedPassword, userId } = result.rows[0];
+
+    const isPasswordCorrect = await bcrypt.compare(password, hashedPassword);
+
+    await createSession(userId);
+    return { message: "Successfully logged in.", success: true };
+  } catch (error) {
+    console.log(error);
+    return {
+      message: "Log in failed. Please try again later.",
       success: false,
     };
   }
