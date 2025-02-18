6c86806c6e4501eecd159628f006e180380216ce
JM Velarde <velarde.jmv8@gmail.com>
2025-02-18 14:55:33 +0300

    Add middleware

--- components/custom/LogoutButton.tsx
+++ components/custom/LogoutButton.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+
+import { useRouter } from "next/navigation";
+
 export default function LogoutButton({
   deleteSession,
 }: {
   deleteSession: () => Promise<void>;
 }) {
+
+  
+  const router = useRouter();
+
   const logOut = async () => {
+    router.push("/");
     await deleteSession();
   };
 
---
+++ middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse, NextRequest } from "next/server";
+import { checkSession } from "./lib/session";
+
+// const privateRoutes = ["/trips/:path"]
+
+export const middleware = async (req:NextRequest) => {
+    const isSession = await checkSession();
+
+    if(isSession) {
+        return NextResponse.next();
+    } else {
+        return NextResponse.redirect(new URL('/login',req.nextUrl))
+    }
+    
+}
+
+export const config = {
+    matcher: ["/trips/:path*"]
+}
